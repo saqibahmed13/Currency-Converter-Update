b0dedb4dd9c0b4c15138c0f6a14f82b4137e1adb
saqibahmed13 <saqiba0484@gmail.com>
2024-02-05 20:02:16 +0530

    added

--- currency.js
+++ currency.js
@@ -1,7 +1,9 @@
 const BASE_URL ="https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies"
 
 const dropdowns = document.querySelectorAll('.dropdown select');
-const btn = document.querySelector("form button")
+
+const btn = document.querySelector("form button");
+
 const fromCurr = document.querySelector('.from select');
 
 const toCurr = document.querySelector('.to select');
